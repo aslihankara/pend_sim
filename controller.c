5e13da09831fef45e1b407fa5dbd264b24bb3f48
James Irwin <irwineffect@gmail.com>
2015-04-21 20:34:52 -0700

    cleaned up the simulator

--- controller.c
+++ controller.c
@@ -1,9 +1,15 @@
 #include "controller.h"
+
+int flop;
+
 int get_action(float x, float x_dot, float theta, float theta_dot, float reinforcement)
 {
-	return 0;
+	flop = !flop;
+	
+	return flop;
 }
 
 void reset_controller(void)
 {
+	flop = 0;
 }
