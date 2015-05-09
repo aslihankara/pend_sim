ab652718ca2f62bcbb774bd2d072995f7027a956
James Irwin <irwineffect@gmail.com>
2015-05-09 00:04:24 -0700

    started neural network learning

--- controller.c
+++ controller.c
@@ -10,8 +10,8 @@
 #define MAX_XD 2
 
 #define EPSILON_INIT .5
-#define LAMBDA .01
-#define GAMMA .2
+#define LAMBDA .1
+#define GAMMA .001
 #define ALPHA .4
 
 #define NUM_T 10
@@ -20,6 +20,8 @@
 #define NUM_XD 10
 #define NUM_ACTIONS 2
 
+#define NUM_INPUTS 4
+
 #define INIT_VALUE 0.0
 
 
@@ -27,6 +29,10 @@ typedef struct QVALS {
 	int t, td, x, xd, a;
 	float value;
 }Qvalues;
+
+typedef struct STATE {
+	double t, td;
+}State;
 
 float q_values[NUM_T][NUM_X][NUM_TD][NUM_XD][NUM_ACTIONS];
 
@@ -43,10 +49,20 @@ const float malpha = ALPHA;
 float *current_state_value;
 float *prev_state_value;
 static double myrandmax;
+int init;
+
+
+double tweights[NUM_INPUTS]; 
+double tdweights[NUM_INPUTS];
 
 //forward declarations
 int myrand(float prob);
 void decay_epsilon(int init);
+State calc_nstate(State cstate, int action);
+double sigmoid(double x);
+void update_weights(State prev, int action, State actual);
+double get_qvalue(State cstate, int action);
+
 
 
 int get_index(float x, float max, int num)
@@ -75,76 +91,177 @@ int get_action(float x, float x_dot, float theta, float theta_dot, float reinfor
 {
 	int i;
 	float max_value;
-	int max_action;
+	int best_action;
 	float current_value;
-	int ix, ixd, it, itd;
+	static State prev_state;
+	static int prev_action;
+	State current_state;
 
 	//convert radians to degrees
 	theta = theta * 180.0/PI;
 	theta_dot = theta_dot * 180.0/PI;
 
-	ix = get_index(x, MAX_X, num_x);
-	ixd = get_index(x_dot, MAX_XD, num_xd);
-	it = get_index(theta, MAX_T, num_t);
-	itd = get_index(theta_dot, MAX_TD, num_td);
+	current_state.t = theta;
+	current_state.td = theta_dot;
 
 	if (!myrand(mepsilon))
 	{
-		max_value = -50.0; //large negative number, pessimistic initialzation
-		max_action = 0;
+		max_value = 50000.0; //large  number, pessimistic initialzation
+		best_action = 0;
 		for (i=0; i < num_actions; ++i)
 		{
-			current_value = q_values[it][ix][itd][ixd][i];
+			current_value = get_qvalue(current_state, i);
 
-			if (current_value > max_value)
+			if (current_value < max_value)
 			{
 				max_value = current_value;
-				max_action = i;
+				best_action = i;
 			}
 		}
 	}
 	else
 	{
-		max_action = rand() % num_actions;
+		best_action = rand() % num_actions;
 	}
 	
-	current_state_value = &q_values[it][ix][itd][ixd][max_action];
-	
-	if(!prev_state_value)
+	if(init)
 	{
-		prev_state_value = current_state_value;
-		return max_action;
+		init = 0;
+		prev_state = current_state;
+		prev_action = best_action; 
+		return best_action; 
 	}
 	
-	
-	//check if terminating state
-	if (reinforcement  <  -0.1)
+		
+	update_weights(prev_state, prev_action, current_state);
+	/*
+	for(i=0; i < NUM_INPUTS; ++i)
 	{
-		for (i=0; i < num_actions; ++i)
-		{
-			q_values[it][ix][itd][ixd][i] = -1;
-		}
+		printf("t[%d]: %lf ", i, tweights[i]);
 	}
+	putchar('\n');
+	for(i=0; i < NUM_INPUTS; ++i)
+	{
+		printf("td[%d]: %lf ", i, tdweights[i]);
+	}
+	putchar('\n');
+	putchar('\n');
+	getchar();
+	*/
 
-	*prev_state_value = *prev_state_value + malpha*(reinforcement + 
-													mgamma * (*current_state_value) - 
-													*prev_state_value);
+	prev_state = current_state;
+	prev_action = best_action; 
 
-	prev_state_value = current_state_value;
-
-	return max_action;
+	return best_action;
 }
+
+
+double get_qvalue(State cstate, int action)
+{
+	State nstate;
+
+	nstate = calc_nstate(cstate, action);
+
+	return sqrt(nstate.t*nstate.t + nstate.td*nstate.td);	
+}
+
+State calc_nstate(State cstate, int action)
+{
+	State next_state;
+	double x;
+	int i;
+	double input[NUM_INPUTS];
+
+	if(!action)
+	{
+		action = -1;
+	}
+
+
+	input[0] = 1; //bias
+	input[1] = cstate.t;
+	input[2] = cstate.td;
+	input[3] = action;
+	
+	for (i=0, x=0; i < NUM_INPUTS; ++i)
+	{
+		x += tweights[i]*input[i];
+	}
+	next_state.t = 2*MAX_T*sigmoid(x) - MAX_T;
+
+
+	for (i=0, x=0; i < NUM_INPUTS; ++i)
+	{
+		x += tdweights[i]*input[i];
+	}
+	next_state.td = 2*MAX_TD*sigmoid(x) - MAX_TD;
+
+
+	return next_state;
+}
+
+void myweightupdate(double error, double weights[NUM_INPUTS], double inputs[NUM_INPUTS])
+{
+	int i;
+
+	for (i=0; i < NUM_INPUTS; ++i)
+	{
+		weights[i] = weights[i] + mgamma*error*inputs[i];
+	}
+}
+
+void update_weights(State prev, int action, State actual)
+{
+	double error;
+	State expected;
+	double inputs[NUM_INPUTS];
+
+	if(!action)
+	{
+		action = -1;
+	}
+
+	inputs[0] = 1;
+	inputs[1] = prev.t;
+	inputs[2] = prev.td;
+	inputs[3] = action;
+
+	expected = calc_nstate(prev, action);
+
+	error = actual.t - expected.t;
+	myweightupdate(error, tweights, inputs);
+
+	error = actual.td - expected.td;
+	myweightupdate(error, tdweights, inputs);
+}
+
+
+double sigmoid(double x)
+{
+	return 1.0/(1+exp(-x));
+}
+
 
 void init_controller(void)
 {
+	int i;
+
 	myrandmax = (double)RAND_MAX;
 	read_states(0);
 	decay_epsilon(1);
+
+	for (i=0; i < NUM_INPUTS; ++i)
+	{
+		tweights[i] = -0.1;
+		tdweights[i] = -0.1;
+	}
+
 }
 
 void reset_controller(void)
 {
 	prev_state_value = 0;
+	init = 1;
 	decay_epsilon(0);
 }
 
@@ -265,6 +382,16 @@ int write_states(char *filename)
 
 	printf("done!\n%ld states written\n", i);
 
+	for(i=0; i < NUM_INPUTS; ++i)
+	{
+		printf("t[%d]: %lf ", i, tweights[i]);
+	}
+	putchar('\n');
+	for(i=0; i < NUM_INPUTS; ++i)
+	{
+		printf("td[%d]: %lf ", i, tdweights[i]);
+	}
+	putchar('\n');
 	fclose(fh);
 
 	return 0;
